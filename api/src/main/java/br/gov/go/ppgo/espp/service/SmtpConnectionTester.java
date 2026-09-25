4b11792333adac001f09be518df01891252113bf
Jorge Miguel <46396207+jhfmiguel@users.noreply.github.com>
2026-09-25 10:24:12 -0300

    Corrige teste de conexão SMTP

--- api/src/main/java/br/gov/go/ppgo/espp/service/SmtpConnectionTester.java
+++ api/src/main/java/br/gov/go/ppgo/espp/service/SmtpConnectionTester.java
@@ -1,5 +1,5 @@
 package br.gov.go.ppgo.espp.service;
 import java.util.Properties; import org.springframework.mail.javamail.JavaMailSenderImpl; import org.springframework.stereotype.Component;
 @Component public class SmtpConnectionTester {
- public void testar(ConfiguracaoEmailService.Envio c){JavaMailSenderImpl s=new JavaMailSenderImpl();s.setHost(c.host());s.setPort(c.porta());s.setUsername(c.remetente());s.setPassword(c.senha());Properties p=s.getJavaMailProperties();p.put("mail.smtp.auth",Boolean.toString(c.autenticacao()));p.put("mail.smtp.starttls.enable",Boolean.toString(c.starttls()));p.put("mail.smtp.starttls.required",Boolean.toString(c.starttls()));p.put("mail.smtp.connectiontimeout","10000");p.put("mail.smtp.timeout","10000");s.testConnection();}
+ public void testar(ConfiguracaoEmailService.Envio c) throws jakarta.mail.MessagingException {JavaMailSenderImpl s=new JavaMailSenderImpl();s.setHost(c.host());s.setPort(c.porta());s.setUsername(c.remetente());s.setPassword(c.senha());Properties p=s.getJavaMailProperties();p.put("mail.smtp.auth",Boolean.toString(c.autenticacao()));p.put("mail.smtp.starttls.enable",Boolean.toString(c.starttls()));p.put("mail.smtp.starttls.required",Boolean.toString(c.starttls()));p.put("mail.smtp.connectiontimeout","10000");p.put("mail.smtp.timeout","10000");s.testConnection();}
 }
