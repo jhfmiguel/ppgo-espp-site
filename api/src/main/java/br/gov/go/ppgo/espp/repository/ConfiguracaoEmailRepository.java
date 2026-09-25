1e90272e325dca3bdb90af1ae14db573473511a4
Jorge Miguel <46396207+jhfmiguel@users.noreply.github.com>
2026-09-25 10:22:07 -0300

    Adiciona configuração SMTP administrativa no padrão NASPP

---
+++ api/src/main/java/br/gov/go/ppgo/espp/repository/ConfiguracaoEmailRepository.java
@@ -0,0 +1,4 @@
+package br.gov.go.ppgo.espp.repository;
+import br.gov.go.ppgo.espp.domain.ConfiguracaoEmail;
+import org.springframework.data.jpa.repository.JpaRepository;
+public interface ConfiguracaoEmailRepository extends JpaRepository<ConfiguracaoEmail,Long> {}
